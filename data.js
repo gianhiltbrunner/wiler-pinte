eb07e419b4a0b2d517d27224c18f5fc354d1a322
protelescristata <gian.silvan.hiltbrunner@gmail.com>
2017-06-17 13:11:43 +0200

    first

--- data.js
+++ data.js
@@ -1,18 +1,73 @@
 var products = {
   "items":[
    {
-     "name":"Annas Beerentraum",
+     "name":"Drink",
      "cost":3.5,
      "amount":0
    },
    {
-     "name":"Bastians Muntermacher",
-     "cost":3.5,
+     "name":"Mineral nature 3dl",
+     "cost":2,
      "amount":0
    },
    {
-     "name":"Cola, Rivella, 3dl",
+     "name":"Cola, Rivella, Eistee 3dl",
      "cost":2.5,
+     "amount":0
+   },
+   {
+     "name":"Mineral nature, 1.5",
+     "cost":7,
+     "amount":0
+   },
+   {
+     "name":"Bier",
+     "cost":4,
+     "amount":0
+   },
+   {
+     "name":"Kaffee, Tee",
+     "cost":3,
+     "amount":0
+   },
+   {
+     "name":"Us dr Güezidose, Brownies, Cookies",
+     "cost":1.5,
+     "amount":0
+   },
+   {
+     "name":"Us dr Bachstube, Schnecke, Muffins",
+     "cost":2.5,
+     "amount":0
+   },
+   {
+     "name":"Nachos",
+     "cost":3,
+     "amount":0
+   },
+   {
+     "name":"Wurst, gross",
+     "cost":6,
+     "amount":0
+   },
+   {
+     "name":"Wurst, klein",
+     "cost":4,
+     "amount":0
+   },
+   {
+     "name":"Glace",
+     "cost":3.5,
+     "amount":0
+   },
+   {
+     "name":"Bretzeli",
+     "cost":0.5,
+     "amount":0
+   },
+   {
+     "name":"Depot",
+     "cost":2,
      "amount":0
    }
  ]
